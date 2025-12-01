2c1e6d6510184cc94a744bd3ecdb12661e920986
Łukasz Piwowar <zsacul@gmail.com>
2025-12-01 08:47:00 +0100

    day01 fin

--- src/day01.rs
+++ src/day01.rs
@@ -5,9 +5,10 @@ pub fn part1(data:&[String])->usize
     data.iter()
         .filter(|s| 
             {
-                let amount = s[1..].parse::<i32>().unwrap();               
+                let amount = s[1..].parse::<i32>().unwrap();
+                let dir = s.chars().next().unwrap();
 
-                if s.chars().next().unwrap()=='L' {
+                if dir=='L' {
                     code -= amount;                                       
                 } else {
                     code += amount;
@@ -29,50 +30,38 @@ pub fn part2(data:&[String])->usize
                 let mut amount = s[1..].parse::<i128>().unwrap();
                 let dir = s.chars().next().unwrap();
 
-                let rot = amount/100;
-                if rot>0 {
-                    res += rot as usize;
+                let full_rot = amount/100;
+
+                if full_rot>0 {
+                    res += full_rot as usize;
                     amount = amount % 100;
                 }
+                let was = code==0;
                 
                 if dir=='L' {
-                    let mut was = code==0;
-                    code -= amount;
-                    
-                    if code==0 {
-                        res+=1;
-                    }
-                    else
-                    {                        
-                        while code<0 {
-                            code+=100;
-
-                            if !was { res+=1;}
-                            was = false;
-                        }                        
-                    }
-                } 
-                  else           
-                {
+                    code -= amount;                   
+                } else {
                     code += amount;
-
-                    if code==0 
-                    {
-                        res+=1;
-                        println!("{} at zero",s);
-                    }
-                      else 
-                    {
-                        while code>99 {
-                            code-=100;
-                            res+=1;                            
-                        }                        
-                    }
                 }              
+
+                if code==0 {
+                    res+=1;
+                }
+                  else
+                {
+                    if code<0 {
+                        code+=100;
+                        if !was { res+=1; }
+                    }                        
+                    if code>99 {
+                        code-=100;
+                        res+=1;                            
+                    }                        
+                }
             }
         );
         
-    res as usize
+    res
 }
 
 #[allow(unused)]
