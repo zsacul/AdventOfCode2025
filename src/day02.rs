ea7d155161c88af39e34f039b5305fdf3f6c5fb2
Łukasz Piwowar <zsacul@gmail.com>
2025-12-02 06:44:55 +0100

    day2 part2 ugly

--- src/day02.rs
+++ src/day02.rs
@@ -1,8 +1,5 @@
-use std::collections::HashMap;
-
-fn valid(s:&str)->Option<i64>
+fn invalid(s:&str)->Option<i64>
 {
-  //  println!("valid {}",s);
     if s.chars().next().unwrap()=='0' 
     {
         return Some(s.parse().unwrap());
@@ -27,8 +24,6 @@ fn valid(s:&str)->Option<i64>
 pub fn part1(data:&[String])->i64
 {
     let tab = data[0].split(',').collect::<Vec<&str>>();
-    let mut code = 0;
-    let mut hash = HashMap::new();
 
     for s in tab.clone()
     {
@@ -42,7 +37,7 @@ pub fn part1(data:&[String])->i64
                 let a = ss[0].parse::<i64>().unwrap();
                 let b = ss[1].parse::<i64>().unwrap();
 
-                let inva:i64 = (a..=b).map(|v| valid(&v.to_string()[..]).unwrap_or(0) as i64)
+                let inva:i64 = (a..=b).map(|v| invalid(&v.to_string()[..]).unwrap_or(0) as i64)
                                   .sum();
                 println!("{} {}",s,inva);
                 inva
@@ -50,53 +45,28 @@ pub fn part1(data:&[String])->i64
         ).sum()
 }
 
-fn valid2(s:&str)->Option<i64>
+fn invalid2(s:&str)->Option<i64>
 {
-  //  println!("valid {}",s);
-    if s.chars().next().unwrap()=='0' 
-    {
-        return Some(s.parse().unwrap());
-    }
-    
-    if s.len()%2!=0
-    {
-        return None;
-    }
-
-
-    for s in 1..=s.len()/2
+    for si in 1..=s.len()/2
     {
         let mut rep="".to_string();
-        while rep
-        if s.chars().nth(s-1).unwrap() >= s.chars().nth(s).unwrap()
+        while rep.len()<s.len()
         {
-            return None;
+            rep += &s[..si];
         }
-    }
-/*
-    for i in 0..s.len()/2
-    {
-        if s.chars().nth(i).unwrap() != s.chars().nth(s.len()/2+i).unwrap() 
-        {
-            return None;
-        }
-    }
-     */
 
-    Some(s.parse().unwrap())
+        if rep==s
+        {
+            return Some(s.parse().unwrap());            
+        }
+    }
+
+    return None;
 }
-
 
 pub fn part2(data:&[String])->i64
 {
     let tab = data[0].split(',').collect::<Vec<&str>>();
-    let mut code = 0;
-    let mut hash = HashMap::new();
-
-    for s in tab.clone()
-    {
-        hash.insert(s, true);
-    }
 
     tab.iter()
        .map(|s| 
@@ -105,9 +75,9 @@ pub fn part2(data:&[String])->i64
                 let a = ss[0].parse::<i64>().unwrap();
                 let b = ss[1].parse::<i64>().unwrap();
 
-                let inva:i64 = (a..=b).map(|v| valid(&v.to_string()[..]).unwrap_or(0) as i64)
+                let inva:i64 = (a..=b).map(|v| invalid2(&v.to_string()[..]).unwrap_or(0) as i64)
                                   .sum();
-                println!("{} {}",s,inva);
+
                 inva
             }
         ).sum()
@@ -137,12 +107,4 @@ fn test2()
         "11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124".to_string()
     ];
     assert_eq!(part2(&v),4174379265);
-}
-
-#[test]
-fn test3()
-{
-    let v = vec![
-    ];
-    assert_eq!(part2(&v),10);
 }
